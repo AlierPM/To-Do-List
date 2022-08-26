b4095e8c57ac766f994475f75022b9028ba1fd93
AlierPM <alierditmaguet@gmail.com>
2022-08-26 22:38:00 +0200

    Add testing on clearCompleted function

--- src/test/add-remove.js
+++ src/test/add-remove.js
@@ -22,3 +22,23 @@ export const updateArray = (taskList) => {
     task.id = taskList.indexOf(task) + 1;
   });
 };
+
+export const editTask = (taskList, id, value) => {
+  for(let i = 0; i<taskList.length; i++) {
+    if (taskList[i].id ==id){
+      taskList[i].description = value;
+    }
+  }
+}
+
+export const completeTask = (taskList, id, status) => {
+  const selected = taskList.findIndex((element) => element.id === id);
+  taskList[selected].completed = status;
+}
+
+export const clearCompletedTask =(taskList) => {
+  taskList = taskList.filter((check) => !check.completed);
+  taskList.forEach((value, index, taskList) => {
+    taskList[index].id = index + 1;
+  });
+};
--- src/test/add-remove.test.js
+++ src/test/add-remove.test.js
@@ -1,4 +1,4 @@
-import { addTask, removeTask } from './add-remove.js';
+import { addTask, removeTask, editTask, completeTask, clearCompletedTask } from './add-remove.js';
 
 // eslint-disable-next-line prefer-const
 let taskArray = [];
@@ -7,7 +7,8 @@ describe('Add', () => {
   test('local storage have two items', () => {
     addTask(taskArray, 'task1');
     addTask(taskArray, 'task2');
-    expect(taskArray).toHaveLength(2);
+    addTask(taskArray, 'task3');
+    expect(taskArray).toHaveLength(3);
   });
 });
 
@@ -15,6 +16,29 @@ describe('remove', () => {
   test('local storage should have one item', () => {
     // addTask(taskArray, 'task1');
     removeTask(taskArray, 1);
-    expect(taskArray).toHaveLength(1);
+    expect(taskArray).toHaveLength(2);
+  });
+});
+
+describe('edit the tasklist', () => {
+test('Editing an Item', () => {
+editTask(taskArray, 1, 'unit testing');
+expect(taskArray[0].description).toBe('unit testing');
+});
+});
+
+describe('task completed', () => {
+test('The task is completed', () => {
+completeTask(taskArray, 0, true);
+completeTask(taskArray, 1, true);
+  expect(taskArray[0].completed).toBe(true);
+  expect(taskArray[1].completed).toBe(true);
+});
+});
+
+describe('Clear completed task', () => {
+  test('A completed task is cleared ', () => {
+    clearCompletedTask(taskArray);
+    expect(taskArray.length).toBe(0);
   });
 });
